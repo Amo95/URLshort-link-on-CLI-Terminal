a5ab61a67afce4ed5850106fd3137e0d1af5b4cc
Amo95 <root@localhost.localdomain>
2020-02-11 16:59:23 +0000

    update

--- url_shortener.py
+++ url_shortener.py
@@ -1,12 +1,18 @@
 # import libraries
 import urllib.request as req
+import sys
+try:
+    # create function
+    def shorturl(url):
+        # set the api of a url shortner to a var
+        apiurl = "http://tinyurl.com/api-create.php?url="
+        shorturl = req.urlopen(apiurl + url).read() #  append parameter to the api
+        return shorturl.decode("utf-8")
 
-# create function
-def shorturl(url):
-    # set the api of a url shortner to a var
-    apiurl = "http://tinyurl.com/api-create.php?url="
-    shorturl = req.urlopen(apiurl + url).read() #  append parameter to the api
-    return shorturl.decode("utf-8")
+    inurl = input("Enter or paste url to shorten here >>> ")
+    BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
-inurl = input("Enter or paste url to shorten here >>> ")
-print(shorturl(inurl))
+    sys.stdout.write(f"\nYour short-linked URL is\n>>> {RED} {shorturl(inurl)} {END}\n")
+
+except KeyboardInterrupt:
+    print("\n\nThank For using this app!!!!")
